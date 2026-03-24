613869e9446c291ec1c835c13fe0620bbd470d7c
Nick Gauthier <nicolas.gauthier@ufl.edu>
2026-03-24 15:35:00 -0400

    add inference to python environmnet

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,19 @@
-utils::globalVariables(c("sahi", "skimage"))
+utils::globalVariables(c("sahi", "skimage", "align_py"))
 
 .onLoad <- function(libname, pkgname) {
   # Declare Python requirements (Reticulate >= 1.41) without initializing Python
   if (utils::packageVersion("reticulate") >= "1.41") {
     reticulate::py_require(c(
-      "sahi", "torch", "torchvision", "opencv-python", "scikit-image"
+      "sahi", 'rfdetr', "inference", "opencv-python", "scikit-image"
     ))
-    reticulate::py_require(
-      "detectron2@git+https://github.com/facebookresearch/detectron2.git@4fa166c043ca45359cd7080523b7122e7e0f9d91"
-    )
   }
 
   # Delay-load Python modules (keeps package load fast + CRAN-safe)
   sahi <<- reticulate::import("sahi", delay_load = TRUE)
   skimage <<- reticulate::import("skimage", delay_load = TRUE)
+  align_py <<- reticulate::import_from_path(
+    "align",
+    path = system.file("python", package = "petrographer"),
+    delay_load = TRUE
+  )
 }
